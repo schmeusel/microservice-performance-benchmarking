c0084b84c3ebbd6fbd31ee7e8990c94e0de384dd
Lukas <meusel.lukas@gmail.com>
2018-03-22 10:32:34 +0100

    Introduce storage manager

---
+++ src/database/StorageManager.ts
@@ -0,0 +1,14 @@
+import { PatternRequestMeasurement } from "../interfaces/index";
+
+class StorageManager {
+    private client: any;
+
+    initialize() {
+        // set up connection to DB
+        throw new Error('initialize not implemented yet.');
+    }
+
+    save(patternRequestMeasurement: PatternRequestMeasurement): void {
+        throw new Error('save not implemented yet.');
+    }
+}
