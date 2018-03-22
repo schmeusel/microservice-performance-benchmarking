7fd8c94af36c132ddfcdeae051ab479f659ac095
Lukas <meusel.lukas@gmail.com>
2018-03-22 09:57:57 +0100

    Lay out experiment controller

---
+++ src/execution/BenchmarkController.ts
@@ -0,0 +1,27 @@
+import { BenchmarkSpecification } from "../interfaces";
+
+class BenchmarkController {
+
+    start(benchmarkSpec: BenchmarkSpecification) {
+        throw new Error("start not implemented.");
+    }
+
+    initializeServices() {
+        // init Swagger and Storage system
+        throw new Error('initializeServices not implemented yet.');
+    }
+
+    preload() {
+        throw new Error('preload not implemented yet.');
+    }
+
+    cleanup() {
+        // depending on what data has been generated, delete it from storage
+        throw new Error('cleanup not implemented yet.');
+    }
+
+    processResults() {
+        // evaluate results, aggregate data
+        throw new Error('processResults not implemented yet.');
+    }
+}
