8f42f6ae5bab3111a55e87fc8267de9c3e639b65
Lukas <meusel.lukas@gmail.com>
2018-05-04 17:56:46 +0200

    Make ExperimentPhase mobile friendlier

--- src/webServer/public/components/ExperimentPhase.js
+++ src/webServer/public/components/ExperimentPhase.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Stepper, Step, StepLabel, StepContent } from 'material-ui';
 import { AsyncPropTypes, StatsPropTypes } from '../constants/CustomPropTypes';
 import { PHASES } from '../constants/ApplicationConstants';
 import PaperContainer from '../containers/PaperContainer';
 
-const ExperimentPhase = ({ phase }) => {
-    function isDisabled(phaseInOrder) {
+export default class ExperimentPhase extends PureComponent {
+    static propTypes = {
+        phase: PropTypes.oneOf(PHASES.ORDER)
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visiblePhase: props.phase
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.phase !== this.props.phase) {
+            this.setState({
+                visiblePhase: nextProps.phase
+            });
+        }
+    }
+
+    isCompleted(phaseName) {
+        const { phase } = this.props;
         const currentIndex = PHASES.ORDER.indexOf(phase);
-        const backgroundIndex = PHASES.ORDER.indexOf(phaseInOrder);
+        const backgroundIndex = PHASES.ORDER.indexOf(phaseName);
         return currentIndex > backgroundIndex;
     }
 
-    function getLabel(phaseName) {
+    getLabel(phaseName) {
+        if (phaseName !== this.state.visiblePhase && phaseName !== this.props.phase) {
+            return null;
+        }
         return phaseName
             .replace('_', ' ')
             .split(' ')
@@ -21,34 +44,47 @@ const ExperimentPhase = ({ phase }) => {
             .join(' ');
     }
 
-    const content = {
-        [PHASES.VALUES.INITIALIZATON]: 'Server just started and all services are being initialized.',
-        [PHASES.VALUES.PATTERN_RESOLUTION]:
-            'The OpenAPI Specification is being analyzed to find suitable resources that can be mapped against from the given abstract patterns.',
-        [PHASES.VALUES.WORKLOAD_GENERATION]:
-            'Based on how abstract patterns and single sequence elements were mapped, requests are being generated with fake parameter data.',
-        [PHASES.VALUES.REQUEST_TRANSMISSION]: 'Now that workloads have been generated, the resulting pattern requests are being fired and measurements taken.',
-        [PHASES.VALUES.MEASUREMENT_EVALUATION]: 'After all requests have been sent, it is time to evaluated the metrics.',
-        [PHASES.VALUES.COMPLETION]:
-            'The benchmarking process is done. In case of manual evaluation, decide now whether you want the integration step to succeed or fail.'
+    onChangeVisible = phaseName => () => {
+        this.setState({
+            visiblePhase: phaseName
+        });
     };
 
-    return (
-        <PaperContainer heading={'Current Phase of the Experiment'}>
-            <Stepper activeStep={PHASES.ORDER.indexOf(phase)}>
-                {PHASES.ORDER.map(phaseName => (
-                    <Step key={phaseName} disabled={isDisabled(phaseName)}>
-                        <StepLabel>{getLabel(phaseName)}</StepLabel>
-                    </Step>
-                ))}
-            </Stepper>
-            <p>{content[phase]}</p>
-        </PaperContainer>
-    );
-};
+    getContent(phaseName) {
+        const content = {
+            [PHASES.VALUES.INITIALIZATON]: 'Server just started and all services are being initialized.',
+            [PHASES.VALUES.PATTERN_RESOLUTION]:
+                'The OpenAPI Specification is being analyzed to find suitable resources that can be mapped against from the given abstract patterns.',
+            [PHASES.VALUES.WORKLOAD_GENERATION]:
+                'Based on how abstract patterns and single sequence elements were mapped, requests are being generated with fake parameter data.',
+            [PHASES.VALUES.REQUEST_TRANSMISSION]:
+                'Now that workloads have been generated, the resulting pattern requests are being fired and measurements taken.',
+            [PHASES.VALUES.MEASUREMENT_EVALUATION]: 'After all requests have been sent, it is time to evaluated the metrics.',
+            [PHASES.VALUES.COMPLETION]:
+                'The benchmarking process is done. In case of manual evaluation, decide now whether you want the integration step to succeed or fail.'
+        };
 
-ExperimentPhase.propTypes = {
-    phase: PropTypes.oneOf(PHASES.ORDER)
-};
+        return content[phaseName];
+    }
 
-export default ExperimentPhase;
+    render() {
+        const { phase } = this.props;
+        const styles = {
+            step: {
+                cursor: 'pointer'
+            }
+        };
+        return (
+            <PaperContainer heading={'Current Phase of the Experiment'}>
+                <Stepper activeStep={PHASES.ORDER.indexOf(phase)}>
+                    {PHASES.ORDER.map(phaseName => (
+                        <Step key={phaseName} completed={this.isCompleted(phaseName)} onClick={this.onChangeVisible(phaseName)} style={styles.step}>
+                            <StepLabel>{this.getLabel(phaseName)}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                <p>{this.getContent(phase)}</p>
+            </PaperContainer>
+        );
+    }
+}
