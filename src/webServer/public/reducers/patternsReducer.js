078aaad02c2e98b5d9bb8efd97f774222c00d623
Lukas <meusel.lukas@gmail.com>
2018-05-12 21:15:11 +0200

    Add coverage for jest

--- src/webServer/public/reducers/patternsReducer.js
+++ src/webServer/public/reducers/patternsReducer.js
@@ -3,10 +3,7 @@ import ActionTypes from '../constants/ActionTypes';
 export default function (state = [], action) {
     switch (action.type) {
         case ActionTypes.PATTERNS: {
-            if (action.data) {
-                return action.data;
-            }
-            return state;
+            return action.data;
         }
     }
     return state;
