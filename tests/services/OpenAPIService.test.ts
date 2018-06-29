6f6f79fc51d3414ea858be7eb6c111c4e89e7f96
Lukas <meusel.lukas@gmail.com>
2018-06-29 23:22:08 +0200

    Update dependencies

--- tests/services/OpenAPIService.test.ts
+++ tests/services/OpenAPIService.test.ts
@@ -1,9 +1,5 @@
-import * as sinon from 'sinon';
 import { expect } from 'chai';
-import * as proxyquire from 'proxyquire';
-
 import OpenAPIService from '../../src/services/OpenAPIService';
-import PatternResolverError from '../../src/exceptions/PatternResolverError';
 import OpenAPIError from '../../src/exceptions/OpenAPIError';
 import { AbstractPatternElementOperation } from '../../src/interfaces';
 import PolyfillUtil from '../../src/utils/PolyfillUtil';
