0b9df09cdb5214ce9355adb4e6859668348f0206
Lukas <meusel.lukas@gmail.com>
2018-06-29 15:29:51 +0200

    Add test for output merging

--- tests/utils/RequestUtil.test.ts
+++ tests/utils/RequestUtil.test.ts
@@ -1,5 +1,43 @@
 import { expect } from 'chai';
+import { buildNewOutputs } from "../../src/utils/RequestUtil";
+import { PatternElementOutputType } from "../../src/interfaces";
+const {ITEM, LIST} = PatternElementOutputType;
 
 describe('Test RequestUtil', () => {
+    describe('buildNewOutputs(...)', () => {
+        it('should return the same output if no outputName was provided', () => {
+            const stubOutputs = 'fake';
+            const result = buildNewOutputs(undefined, ITEM, stubOutputs, {});
+            expect(result).to.equal(stubOutputs);
+        });
 
+        it('should merge new output into existing structure', () => {
+            const outputNameStub = 'newEntry';
+            const valueStub = 1;
+            const outputStub = {
+                ['test']: {
+                    [LIST]: valueStub
+                },
+                [outputNameStub]: {
+                    [LIST]: valueStub
+                },
+            };
+            const responseStub = {
+                body: valueStub
+            };
+
+            const expectedOutput = {
+                ['test']: {
+                    [LIST]: valueStub
+                },
+                [outputNameStub]: {
+                    [LIST]: valueStub,
+                    [ITEM]: valueStub
+                }
+            };
+
+            const result = buildNewOutputs(outputNameStub, ITEM, outputStub, responseStub);
+            expect(result).to.deep.equal(expectedOutput);
+        })
+    });
 });
